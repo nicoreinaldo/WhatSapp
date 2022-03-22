f2e3020db77bd99b05d36854e0d8e600a6c8c755
nicolas <nicoreinaldo@hotmail.com>
2022-03-22 17:08:36 -0300

    starting application

---
+++ src/main/java/com/nulpointerexception/whatsapp/whatSappApplication.java
@@ -0,0 +1,13 @@
+package com.nulpointerexception.whatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class whatSappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(whatSappApplication.class, args);
+	}
+
+}
